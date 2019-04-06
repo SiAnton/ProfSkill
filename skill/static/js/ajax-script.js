1493615e3b80e605c2939438852112a589ee72e3
SiAnton <ant.sinash@gmail.com>
2019-04-07 00:43:41 +0700

    end-13 new python

--- skill/static/js/ajax-script.js
+++ skill/static/js/ajax-script.js
@@ -1,19 +1,19 @@
 
-window.onload = function () {
-    $("#form").submit(function (e) {
-        e.preventDefault();
+// window.onload = function () {
+//     $("#form").submit(function (e) {
+//         e.preventDefault();
 
-        var data = $("#category").val();
-        console.log(data)
+//         var data = $("#category").val();
+//         console.log(data)
 
-        $.ajax({
-            url: '/service/',
-            type: 'GET',
-            data: $("#category").val(),
-            cache: false,
-            success: function () {
-                console.log("yes");
-            },
-        });
-    });
-}
+//         $.ajax({
+//             url: '/service/',
+//             type: 'GET',
+//             data: $("#category").val(),
+//             cache: false,
+//             success: function () {
+//                 console.log("yes");
+//             },
+//         });
+//     });
+// }
